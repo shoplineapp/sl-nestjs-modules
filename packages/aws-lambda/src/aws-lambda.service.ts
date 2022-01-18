eba9c0be5b5aa8850a822607e6e8f0d9f90b2223
SzeLong <szelong@shoplineapp.com>
2022-01-18 12:33:30 +0800

    docs(aws-lambda): add minor details

--- packages/aws-lambda/src/aws-lambda.service.ts
+++ packages/aws-lambda/src/aws-lambda.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InvokeCommand, LambdaClient } from '@aws-sdk/client-lambda';
 import { AwsLambdaFunctionError, AwsLambdaResponseError } from './aws-lambda.errors';
 
+/** Service class providing interface to AWS Lambda */
 @Injectable()
 export class AwsLambdaService {
   constructor(private readonly client: LambdaClient) {}
 
   /**
    * Invoke a AWS Lambda function
-   * @param functionName Name of the AWS Lambda function
+   * @param functionName Either the ARN or the name of the AWS Lambda function. See [here](https://docs.aws.amazon.com/lambda/latest/dg/API_Invoke.html#API_Invoke_RequestSyntax) for more details
    * @param payload Payload to send to the AWS Lambda function
    * @returns Payload responded from the AWS Lambda function
    */
