71b445882f05426887296951d96caccc2ff18df5
Andes Lam <andes@shoplineapp.com>
2022-01-28 14:44:43 +0800

    fix: indentation

--- packages/google-analytics/src/google-analytics.options.interface.ts
+++ packages/google-analytics/src/google-analytics.options.interface.ts
@@ -1,8 +1,8 @@
-  /**
-   * @param measurementId Mandatory measurement ID, part of the mandatory URL parameters.
-   * @param secret API secret, part of the mandatory URL parameters.
-   * For more details of all parameters, see [here](https://developers.google.com/analytics/devguides/collection/protocol/ga4/sending-events?client_type=gtag#required_parameters)
-   */
+/**
+ * @param measurementId Mandatory measurement ID, part of the mandatory URL parameters.
+ * @param secret API secret, part of the mandatory URL parameters.
+ * For more details of all parameters, see [here](https://developers.google.com/analytics/devguides/collection/protocol/ga4/sending-events?client_type=gtag#required_parameters)
+ */
 export interface GoogleAnalyticsConfigOption {
   measurementId: string;
   secret: string;
